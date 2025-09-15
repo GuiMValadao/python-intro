ee99297d612998771d0f6889cb165bc712530bd0
GuiMValadao <guimvaladao@yahoo.com.br>
2025-09-15 16:40:56 -0300

    execicio cap18

---
+++ arquivos_de_exemplos_e_testes/exercicios/cap_18_exercise.py
@@ -0,0 +1,77 @@
+#########################################################################
+# The aim of this exercise is to create a new class called Account.
+#1. Define a new class to represent a type of bank account.
+#2. When the class is instantiated you should provide the account number, the name
+#of the account holder, an opening balance and the type of account (which can be
+#a string representing 'current', 'deposit' or 'investment' etc.). This means that
+#there must be an __init__ method and you will need to store the data within
+#the object.
+#3. Provide three instance methods for the Account; deposit(amount),
+#withdraw(amount) and get_balance(). The behaviour of these
+#methods should be as expected, deposit will increase the balance, withdraw will
+#decrease the balance and get_balance() returns the current balance.
+#4. Define a simple test application to verify the behaviour of your Account class.
+#It can be helpful to see how your class Account is expected to be used. For this
+#reason a simple test application for the Account is given below:
+#The following output illustrates what the result of running this test application
+#might look like:
+#acc1 = Account('123', 'John', 10.05, 'current')
+#acc2 = Account('345', 'John', 23.55, 'savings')
+#acc3 = Account('567', 'Phoebe', 12.45, 'investment')
+#print(acc1)
+#print(acc2)
+#print(acc3)
+#acc1.deposit(23.45)
+#acc1.withdraw(12.33)
+#print('balance:', acc1.get_balance())
+#Account[123] - John, current account = 10.05
+#Account[345] - John, savings account = 23.55
+#Account[567] - Phoebe, investment account = 12.45
+#balance: 21.17
+##########################################################################
+
+class Conta():
+    """ Esta classe cria objetos que contém informações
+    a respeito de uma conta bancária contendo as seguintes informações:
+    número da conta; nome do titular da conta; um saldo de abertura
+    e o tipo da conta.
+    """
+    def __init__(self, numero, nome, saldo, tipo):
+        """ Inicializa o objeto 'Conta' e salva suas informações. """
+        self.numero = numero
+        self.nome = nome
+        self.saldo = saldo
+        self.tipo = tipo
+    
+    def __str__(self):
+        return self.nome + ', conta ' + self.tipo + ' = R$ ' + str(self.saldo)
+    def depositar(self, valor):
+        self.saldo = float(self.saldo) + float(valor)
+        return self.saldo
+
+    def saque(self, valor):
+        self.saldo = float(self.saldo) - float(valor)
+        return self.saldo
+    
+    def obter_saldo(self):
+        print('Seu saldo atual é de R$', self.saldo)
+        return
+
+
+
+c1 = Conta('123', 'João', 340, 'corrente')
+c2 = Conta('456', 'João', 20.23, 'depósito')
+c3 = Conta('789', 'Carlo', 2340.54, 'investimento')
+
+print('Titular:', c1.nome)
+print('Número da conta:', c1.numero)
+print('Saldo:', c1.saldo)
+print('Tipo de conta:', c1.tipo)
+c1.depositar(245.23)
+print('Saldo:', c1.saldo)
+c1.saque(105.04)
+print('Saldo:', c1.saldo)
+c1.obter_saldo()
+print(c1)
+print(c2)
+print(c3)
