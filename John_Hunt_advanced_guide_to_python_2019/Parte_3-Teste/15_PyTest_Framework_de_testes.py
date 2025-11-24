f33be87d75701969fdaa1f6a41c0fdfe88b8dbcf
Guilherme <guimvaladao@yahoo.com.br>
2025-11-24 17:21:46 -0300

    cap 15

--- John_Hunt_advanced_guide_to_python_2019/Parte_3-Teste/15_PyTest_Framework_de_testes.py
+++ John_Hunt_advanced_guide_to_python_2019/Parte_3-Teste/15_PyTest_Framework_de_testes.py
@@ -126,3 +126,127 @@
 #     calc.set(1)
 #     calc.add()
 #     assert calc.total == 2
+
+# Agora temos 4 testes para executar (poderíamos ir além, mas isto é suficiente
+# por hora). Um dos problemas com este conjunto de testes é que repetimos
+# a criação do objeto Calculadora em cada início de cada teste. Enquanto
+# isto não é um problema em si, ele resulta em código duplicado e na possibilidade
+# de problemas futuros em termos de manutenção se quisermos alterar o modo
+# com que uma calculadora é criada. Também pode não ser tão eficiente quanto
+# reutilizar o objeto Calculadora para cada teste.
+# Podemos, entretanto, definir um acessório que pode ser executado antes
+# de cada teste de função individual ser executado. Para fazer isso, escrevemos
+# uma nova função e usamos o decorador pytest.fixture naquela função. Isto
+# marca a função como sendo especial e que pode ser usada como um acessório
+# em uma função individual.
+# Funções que requerem o acessório deveriam aceitar uma referência ao acessório
+# como um argumento para a função teste individual. Por exemplo, para um teste
+# aceitar um acessório chamado calculadora; deveria ter um argumento com o
+# nomde do acessório, isto é, calculadora. Este nome pode, então, ser usado para
+# acessar o objeto retornado. Isto é ilustrado abaixo:
+# import pytest
+# from calculadora import Calculadora
+
+# @pytest.fixture
+# def calculadora():
+#     """ Retorna uma instância de Calculadora"""
+#     return Calculadora()
+
+# def test_initial_value(calculadora):
+#     assert calculadora.total == 0
+
+# def test_add_one(calculadora):
+#     calculadora.set(1)
+#     calculadora.add()
+#     assert calculadora.total == 1
+
+# def test_subtract_one(calculadora):
+#     calculadora.set(1)
+#     calculadora.sub()
+#     assert calculadora.total == -1
+
+# def test_add_one_and_one(calculadora):
+#     calculadora.set(1)
+#     calculadora.add()
+#     calculadora.set(1)
+#     calculadora.add()
+#     assert calculadora.total == 2
+
+# No código acima, cada uma das funções teste aceita o acessório caculadora
+# que é usado para instancias o objeto Calculadora. Portanto, de-duplicamos
+# nosso código; agora há apenas um pedaço de código que define como um objeto
+# calculadora deveria ser criado para nossos testes. Note que cada teste é
+# fornecido com uma instância completamente nova do objeto Calculadora; não há,
+# portanto, nenhuma chance de um teste impactar outro teste.
+# Também é considerado boa prática acrescentar uma docstring para seus
+# acessórios como foi feito. Isto é pois PyTest pode criar uma lista de
+# todos os acessórios disponíveis junto com suas docstrings. Da linha de comando,
+# insto é feito usando:
+# >pytest fixtures
+# Os acessórios de PyTest podem ser aplicados a funções (como acima), classes,
+# módulos, pacotes ou sessões. O alcance de um acessório pode ser indicado pelo
+# parâmatro (opcional) scope ao decorador do acessório. O alcence determina
+# em qual ponto um acessório deveria ser executado. Por exemplo, um acessório
+# com escopo 'sessão' será executado uma vez para a sessão de teste, um acessório
+# com escopo 'módulo' será executado uma vez para o módulo, um acessório com
+# alcance de classe indica um acessório que é executado para cada nova instância
+# de uma classe de teste criada etc.
+# Outro parâmetro para o decorador acessório é autouse, que, se definido como
+# True, ativará o acessório para todos os testes que pode ver. Se estiver definido
+# como False (o padrão), então uma referência explícita em uma função teste
+# (ou método) é exigida para ativar o acessório.
+# Se adicionarmos alguns acessórios extras para os testes podemos ver quando
+# são executados (alterado no arquivo test_calculadora.py). Para ver a saída dos
+# testes, executar pelo terminal usando:
+# > pytest -s path\test_calculadora.py.
+# -----------------------------------------------------
+# Testes parametrizados
+# Um requisito comum de um teste é executar o mesmo teste múltiplas vezes
+# com vários valores de entrada diferentes. Isto pode reduzir significativamente
+# o número de testes que devem ser definidos. Tais testes são referidos como
+# testes parametrizados; com os valores de parâmetro para o teste especificados
+# usando o decorador @pytest.mark.parametrize.
+# @pytest.mark.parametrize('input1, input2, expected', [(3,1,4), (3,2,5),])
+# def test_calculadora_add_operacao(calculadora, input1, input2, expected):
+#     calculadora.set(input1)
+#     calculadora.add()
+#     calculadora.set(input2)
+#     calculadora.add()
+#     assert calculadora.total == expected
+# Isto ilustra a preparação de um teste parametrizado para a Calculadora no
+# qual dois valores são somados e comparados com o resultado esperado. Note
+# que os parâmetros são nomeados no decorador e então uma lista de tuplas é
+# usada para definir os valores a serem usados para os parâmetros. Neste caso,
+# o test_calculadora_add_operacao será executado duas vezes.
+#
+#  Testando para Exceções
+#
+# Pode-se escrever testes que verificam que uma exceção foi levantada. Isto
+# é útil pois testar comportamento negativo é tão importante quanto testar
+# comportamento positivo. Por exemplo, poderíamos querer verificar que uma
+# exceção particular foi levantada quando tentamos sacar dinheiro de uma conta
+# bancária que nos permitirá pegar valores acima do limite. Para verificar a
+# presença de uma exceção em PyTest, usa-se a declaração 'with' e pytest.raises.
+# Isto é um gerenciador de contexto que verificará na saída que a exceção
+# especificada foi levantada. É usada como segue:
+# with pytest.raises(accounts.BalanceError):
+#     current_account.withdraw(200.0)
+#
+# Ignorando testes
+#
+# Em alguns casos, é útil escrever um teste para funcionalidades que ainda não
+# foram implementadas; isto pode ser para garantir que um teste não é esquecido
+# ou pois ajuda a documentar o que o item a ser testado deveria fazer. Entretanto,
+# se o teste é executado então o conjunto de testes inteiro irá falhar pois
+# o teste está sendo executado com comportamento que ainda será escrito.
+# Uma maneira de lidar com esse problema é decorar um teste com o decorador
+# @pytest.mark.skip:
+# @pytest.mark.skip(reason='not implemented yet')
+# def test_calculator_multiply(calculadora):
+#     calculadora.multiply(2, 3)
+#     assert calculadora.total == 6
+# Isto indica que PyTest deveria guardar a presença do teste mas não deveria
+# tentar executá-lo. PyTest notará então que o teste foi pulado.
+# É geralmente considerado boa prática fornecer uma razão para a qual o teste
+# foi pulado de modo que fica mais fácil manter controle. Esta informação também
+# fica disponível quando PyTest pula o teste.
--- John_Hunt_advanced_guide_to_python_2019/Parte_3-Teste/test_calculadora.py
+++ John_Hunt_advanced_guide_to_python_2019/Parte_3-Teste/test_calculadora.py
@@ -1,29 +1,115 @@
-from calculadora import Calculadora, increment
+# from calculadora import Calculadora, increment
 
 
-def test_initial_value():
-    calc = Calculadora()
-    assert calc.total == 0
+# def test_initial_value():
+#     calc = Calculadora()
+#     assert calc.total == 0
 
 
-def test_subtract_one():
-    calc = Calculadora()
-    calc.set(1)
-    calc.sub()
-    assert calc.total == -1
+# def test_subtract_one():
+#     calc = Calculadora()
+#     calc.set(1)
+#     calc.sub()
+#     assert calc.total == -1
 
 
-def test_add_one():
-    calc = Calculadora()
-    calc.set(1)
-    calc.add()
-    assert calc.total == 1
+# def test_add_one():
+#     calc = Calculadora()
+#     calc.set(1)
+#     calc.add()
+#     assert calc.total == 1
 
 
-def test_add_one_and_one():
-    calc = Calculadora()
-    calc.set(1)
-    calc.add()
-    calc.set(1)
-    calc.add()
-    assert calc.total == 2
+# def test_add_one_and_one():
+#     calc = Calculadora()
+#     calc.set(1)
+#     calc.add()
+#     calc.set(1)
+#     calc.add()
+#     assert calc.total == 2
+
+from typing import Literal
+import pytest
+from calculadora import Calculadora
+
+
+@pytest.fixture(scope="session", autouse=True)
+def session_scope_fixture():
+    print("\nsession_scope_fixture")
+    yield
+    print("exiting session fixture")
+
+
+@pytest.fixture(scope="module", autouse=True)
+def module_scope_fixture():
+    print("module_scope_fixture")
+    yield
+    print("exiting module fixture")
+
+
+@pytest.fixture(scope="class", autouse=True)
+def class_scope_fixture():
+    print("class_scope_fixture")
+    yield
+    print("exiting class fixture")
+
+
+@pytest.fixture()
+def calculadora():
+    """Retorna uma instância de Calculadora"""
+    print("calculadora fixture")
+    return Calculadora()
+
+
+def test_initial_value(calculadora: Calculadora):
+    print("\ninitial_value")
+    assert calculadora.total == 0
+
+
+def test_add_one(calculadora: Calculadora):
+    print("\nadd_one")
+    calculadora.set(1)
+    calculadora.add()
+    assert calculadora.total == 1
+
+
+def test_subtract_one(calculadora: Calculadora):
+    print("\nsubtract_one")
+    calculadora.set(1)
+    calculadora.sub()
+    assert calculadora.total == -1
+
+
+def test_add_one_and_one(calculadora: Calculadora):
+    print("\nadd_one_and_one")
+    calculadora.set(1)
+    calculadora.add()
+    calculadora.set(1)
+    calculadora.add()
+    assert calculadora.total == 2
+
+
+@pytest.mark.parametrize(
+    "input1, input2, expected",
+    [
+        (3, 1, 4),
+        (3, 2, 5),
+    ],
+)
+def test_calculadora_add_operacao(
+    calculadora: Calculadora,
+    input1: Literal[3],
+    input2: Literal[1] | Literal[2],
+    expected: Literal[4] | Literal[5],
+):
+    calculadora.set(input1)
+    calculadora.add()
+    calculadora.set(input2)
+    calculadora.add()
+    assert calculadora.total == expected
+
+
+@pytest.mark.skip(reason="not implemented yet")
+def test_calculator_multiply(calculadora: Calculadora):
+    calculadora.multiply(2, 3)
+    assert calculadora.total == 6
