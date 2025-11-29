eb67827f96b17859d6255ee5623f10c2d5b2660d
Guilherme <guimvaladao@yahoo.com.br>
2025-11-29 15:45:17 -0300

    teste de calculadora com pytest

--- John_Hunt_advanced_guide_to_python_2019/Parte_3-Teste/exercicios/cap15/calculadora.py
+++ John_Hunt_advanced_guide_to_python_2019/Parte_3-Teste/exercicios/cap15/calculadora.py
@@ -1,43 +1,94 @@
 class Calculadora:
 
-    def __init__(self, valor1, valor2, operador="+"):
-        self.valor1 = valor1
-        self.valor2 = valor2
+    def __init__(
+        self,
+    ):
+        self._valor1 = 0
+        self._valor2 = 0
         self.total = 0
-        self.escolher_operador(operador, valor1, valor2)
+        self._operador = ""
 
-    def escolher_operador(self, operador):
+    @property
+    def operador(self):
+        return self._operador
+
+    @operador.setter
+    def operador(self, escolha_usuario):
+
         operadores = ["+", "-", "*", "/"]
-        operacoes = [self.soma, self.sub, self.mult, self.div]
-        if operador in operadores:
-            i = operadores.index(operador)
-            operacoes[i](self)
-
+        if escolha_usuario in operadores:
+            self._operador = escolha_usuario
         else:
-            print("Operação escolhida não registrada.")
+            print("Operação escolhida inválida!")
+            self._operador = None
 
-    def soma(self):
-        resultado = self.valor1 + self.valor2
-        self.total += resultado
+    @property
+    def valor1(self):
+        return self._valor1
+
+    @property
+    def valor2(self):
+        return self._valor2
+
+    @valor1.setter
+    def valor1(self, valor):
+        escolha_usuario = valor
+        try:
+            usuario_float = float(escolha_usuario)
+            self._valor1 = usuario_float
+
+        except ValueError:
+            print("Valor escolhido não é um número.")
+            self._valor1 = None
+
+    @valor2.setter
+    def valor2(self, valor):
+        escolha_usuario = valor
+        try:
+            usuario_float = float(escolha_usuario)
+            self._valor2 = usuario_float
+        except ValueError:
+            print("Valor escolhido não é um número.")
+            self._valor2 = None
+
+    def realizar_operacao(self):
+        if self.operador == "+":
+            return self._soma()
+        elif self.operador == "-":
+            return self._sub()
+        elif self.operador == "*":
+            return self._mult()
+        elif self.operador == "/":
+            return self._div()
+        else:
+            return None
+
+    def _soma(self):
+        resultado = self._valor1 + self._valor2
+        # self.total += resultado
         return resultado
 
-    def sub(self):
+    def _sub(self):
         resultado = self.valor1 - self.valor2
-        self.total += resultado
+        # self.total += resultado
         return resultado
 
-    def mult(self):
+    def _mult(self):
         resultado = self.valor1 * self.valor2
-        self.total += resultado
+        # self.total += resultado
         return resultado
 
-    def div(self):
+    def _div(self):
         try:
             resultado = self.valor1 / self.valor2
-            self.total += resultado
+            # self.total += resultado
             return resultado
         except ZeroDivisionError:
             print("Divisão por zero, cálculo interrompido.")
+            return None
+
+    def mudar_total(self, valor):
+        self.total += valor
 
     def total_atual(self):
         return self.total
--- John_Hunt_advanced_guide_to_python_2019/Parte_3-Teste/exercicios/cap15/test_calculadora.py
+++ John_Hunt_advanced_guide_to_python_2019/Parte_3-Teste/exercicios/cap15/test_calculadora.py
@@ -3,26 +3,112 @@ import pytest
 
 
 @pytest.fixture()
-def calculadora(valor1, valor2):
+def calculadora():
     """Retorna uma instância de Calculadora"""
-    return Calculadora(valor1, valor2)
+    return Calculadora()
 
 
-def test_soma():
-    resultado = calculadora(operador="+")
-    assert resultado == 2
+@pytest.mark.parametrize(
+    "input1, input2, expected",
+    [(1, 1, 2), (-1, 1, 0), (10, 10, 20)],
+)
+def test_soma(calculadora: Calculadora, input1, input2, expected):
+    calculadora.operador = "+"
+    calculadora.valor1 = input1
+    calculadora.valor2 = input2
+    soma = calculadora.realizar_operacao()
+    assert soma == expected
 
 
-def test_sub():
-    resultado = calculadora(operador="-")
-    assert resultado == 0
+@pytest.mark.parametrize(
+    "input1, input2, expected",
+    [
+        (1, 1, 0),
+        (-1, 1, -2),
+        (20, 10, 10),
+        (10, 20, -10),
+    ],
+)
+def test_sub(calculadora: Calculadora, input1, input2, expected):
+    calculadora.operador = "-"
+    calculadora.valor1 = input1
+    calculadora.valor2 = input2
+    subtracao = calculadora.realizar_operacao()
+    assert subtracao == expected
 
 
-def test_mult():
-    resultado = calculadora(operador="*")
-    assert resultado == 1
+@pytest.mark.parametrize(
+    "input1, input2, expected",
+    [
+        (1, 1, 1),
+        (-1, 1, -1),
+        (5, 5, 25),
+        (0, 10, 0),
+    ],
+)
+def test_mult(calculadora: Calculadora, input1, input2, expected):
+    calculadora.operador = "*"
+    calculadora.valor1 = input1
+    calculadora.valor2 = input2
+    mult = calculadora.realizar_operacao()
+    assert mult == expected
 
 
-def test_div():
-    resultado = calculadora(operador="/")
-    assert resultado == 1
+@pytest.mark.parametrize(
+    "input1, input2, expected",
+    [(1, 1, 1), (-1, 1, -1), (10, 2, 5), (10, 0, None)],
+)
+def test_div(calculadora: Calculadora, input1, input2, expected):
+    calculadora.operador = "/"
+    calculadora.valor1 = input1
+    calculadora.valor2 = input2
+    div = calculadora.realizar_operacao()
+    assert div == expected
+
+
+@pytest.mark.parametrize(
+    "input1, input2, expected",
+    [(1, 1, 4), (-1, 1, 0), (10, 10, 40)],
+)
+def test_memo(calculadora: Calculadora, input1, input2, expected):
+    calculadora.operador = "+"
+    calculadora.valor1 = input1
+    calculadora.valor2 = input2
+    soma = calculadora.realizar_operacao()
+    calculadora.mudar_total(soma)
+    calculadora.mudar_total(soma)
+    assert calculadora.total == expected
+
+
+@pytest.mark.parametrize(
+    "input1, input2, expected",
+    [(1, 1, 0), (-1, 1, 0), (10, 10, 0)],
+)
+def test_limpar_memo(calculadora: Calculadora, input1, input2, expected):
+    calculadora.operador = "+"
+    calculadora.valor1 = input1
+    calculadora.valor2 = input2
+    soma = calculadora.realizar_operacao()
+    calculadora.mudar_total(soma)
+    calculadora.limpar_total()
+    assert calculadora.total == expected
+
+
+@pytest.mark.parametrize(
+    "input1, input2, expected",
+    [("a", 1, (None, 1)), (-1, "v", (-1, None)), (10, 10, (10, 10))],
+)
+def test_inputs(calculadora: Calculadora, input1, input2, expected):
+    calculadora.valor1 = input1
+    calculadora.valor2 = input2
+    valor1, valor2 = calculadora.valor1, calculadora.valor2
+    assert (valor1, valor2) == expected
+
+
+@pytest.mark.parametrize(
+    "operador, expected",
+    [("+", "+"), ("-", "-"), ("*" "*"), ("/", "/"), ("a", None), ("%", None)],
+)
+def test_inputs_operadores(calculadora: Calculadora, operador, expected):
+    calculadora.operador = operador
+    assert calculadora.operador == expected
